3b5a23ee003fd501520d00ee9f99b93cb6ccbfa6
nathapon <hellonathapon@gmail.com>
2022-03-05 08:16:04 +0700

    Fix cors issue

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(passport.initialize());
 
 
 // Middlewares
-app.use(cors())
+// app.use(cors())
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Authorization, Accept, Origin, Access-Control-Allow-Headers, X-Requested-With, content-type, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser())
@@ -24,6 +34,9 @@ app.use(cookieParser())
 app.use('/login', require('./router/auth/login'));
 app.use('/api/customer', require('./router/api/customer'));
 app.use('/api/address', require('./router/api/address'));
+app.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.status(200).json({message: 'Authorized'})
+})
 
 
 const server = app.listen(process.env.PORT || 5000, () => {
